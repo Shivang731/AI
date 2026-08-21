c0e80a1b46af6afba7ba0c229cf519082bf5cbf2
Shivang <shivangdubey731@gmail.com>
2026-08-21 23:58:27 +0530

    fix : founduser stored properly

--- Auth(Week-6)/index.js
+++ Auth(Week-6)/index.js
@@ -5,48 +5,52 @@ app.use(express.json());
 
 const users = [];
 
-function generateToken(){
-    return Math.random()
+function generateToken() {
+    return Math.random();
 }
 
+app.post("/signup", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.post("/signup",function(req,res){
-     const username = req.body.username;
-     const password = req.body.password;
-
-    users.push ({
+    users.push({
         username: username,
         password: password
-    })
+    });
 
     res.json({
-        message:"You are signed in"
-    })
+        message: "You are signed in"
+    });
+    console.log(users);
 });
 
-app.post("/signin",function(req,res){
+app.post("/signin", function(req, res) {
     const username = req.body.username;
-     const password = req.body.password;
+    const password = req.body.password;
 
-     const founduser = users.find(function(u)){
-        if(founduser) {
-            
-       const token = generateToken();
-        res.json ({
-            token :token
+    const foundUser = users.find(function(u) {
+        return u.username == username && u.password == password;
+    });
+
+    if (foundUser) {
+        const token = generateToken();
+        foundUser.token = token;
+        res.json({
+            token : token
         });
-        } else {
-            res.json({
-                "Invalid username or password"
-            });
-        }
-            
-    
-     }
-
-
+    } else {
+        res.json({
+            message: "Invalid username or password"
+        });
+    }
+    console.log(users);
 });
 
-app.listen(3000, ()=> {
-    console.log("Server is running on localhost")
+app.get("/me",function(req,res){
+    const token = req.headers.token
+    const foundUser = 
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on localhost");
 });
